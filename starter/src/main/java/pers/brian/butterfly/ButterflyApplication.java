1f536427a845d39c51f9e8d52a26fbf6dbcb55b2
hukaiyang <hukaiyang@huice.com>
2021-03-29 15:22:01 +0800

    '初始化项目'

---
+++ starter/src/main/java/pers/brian/butterfly/ButterflyApplication.java
@@ -0,0 +1,16 @@
+package pers.brian.butterfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hukaiyang
+ * @date 2021-03-26 16:51
+ **/
+@SpringBootApplication
+public class ButterflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButterflyApplication.class, args);
+	}
+}
